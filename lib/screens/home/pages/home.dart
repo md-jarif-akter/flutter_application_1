a63efccf9946912248e047303f77ca0f8cf384d4
md-jarif-akter <akter15-5156@diu.edu.bd>
2025-03-11 10:19:41 +0600

    eCfp-1

---
+++ lib/screens/home/pages/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Mega Shop",
+          style: TextStyle(
+            fontFamily: 
+          )
+        
+      )
+    )
+    return const Placeholder();
+  }
+}
---
+++ lib/utils/constant/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FColors {
+  static const primaryBlueOcean = Color(0xFF5CC936);
+  static const primaryBlueOcean = Color(0xFFFFC120);
+  static const primaryBlueOcean = Color(0xFF0C1A30);
+
+}
